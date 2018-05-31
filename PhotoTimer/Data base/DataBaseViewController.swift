8f380f1c7edb0c8eddd56fe0ddf09f07b908795f
Evgeny Evtushenko <4eshire.s@gmail.com>
2018-05-31 10:34:09 +0500

    Натянул половину дизайа

--- PhotoTimer/Data base/DataBaseViewController.swift
+++ PhotoTimer/Data base/DataBaseViewController.swift
@@ -22,12 +22,22 @@ class DataBaseViewController: UIViewController {
     var configurations: [RealmDevelop] = []
     var configurationsList: [RealmDevelop] = []
     
+    private lazy var backgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 5.0
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 1
+        view.layer.shadowOffset = CGSize.zero
+        view.layer.shadowRadius = 10
+        return view
+    }()
+    
 
     //MARL: Initialization
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        loadSamplesFromNetworkToDB()
         loadSamplesFromDB()
         realm = try! Realm()
         self.tableview?.reloadData()
@@ -35,6 +45,12 @@ class DataBaseViewController: UIViewController {
         
         let longpress = UILongPressGestureRecognizer(target: self, action: #selector(longPressGestureRecognized))
         tableview.addGestureRecognizer(longpress)
+        
+        tableview.backgroundColor = UIColor.white
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        
     }
     
     //MARK: Actions
@@ -161,20 +177,6 @@ class DataBaseViewController: UIViewController {
     }
     
     //MARK: Private functions
-    func loadSamplesFromNetworkToDB() {
-        ProductsInteractor().getProducts { configurations in
-            self.configurations = configurations
-            
-            for item in configurations {
-                try! self.realm.write {
-                    self.realm.add(item)
-                }
-            }
-        }
-//        loadSampleTimers()
-//        self.tableview.reloadData()
-    }
-    
     private func loadSamplesFromDB() {
         let realm = try! Realm()
         var configurations = [RealmDevelop]()
@@ -192,6 +194,24 @@ extension DataBaseViewController: UITableViewDelegate, UITableViewDataSource {
         return 1
     }
     
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        let headerLabel = UILabel(frame: CGRect(x: 30, y: 0, width:
+            tableView.bounds.size.width, height: tableView.bounds.size.height))
+        headerLabel.font = UIFont(name: ".SFUIText-Semibold", size: 37)
+        headerLabel.textColor = UIColor.black
+        headerLabel.text = self.tableView(tableView, titleForHeaderInSection: section)
+        headerLabel.sizeToFit()
+        headerView.addSubview(headerLabel)
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        tableview.headerView(forSection: 0)?.textLabel?.font = UIFont(name: ".SFUIText-Semibold", size: CGFloat(55))
+        return "Таймеры"
+    }
+    
     // Override to support conditional editing of the table view.
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
@@ -203,6 +223,10 @@ extension DataBaseViewController: UITableViewDelegate, UITableViewDataSource {
         return configurationsList.count
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90.0;
+    }
+    
     //Определяем содержимое ячеек
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let configuration = configurationsList[indexPath.row]
@@ -210,10 +234,25 @@ extension DataBaseViewController: UITableViewDelegate, UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? DataBaseTableViewCell else {
             fatalError("The dequeued cell is not an instance of DataBaseTableViewCell.")
         }
+        
         guard let timerName = configuration.schemeName else {
             fatalError("Timer name is nil in row with index \(indexPath.row)")
         }
+        
+        cell.contentView.backgroundColor = UIColor.clear
         cell.timerNameTextLabel?.text = timerName
+        cell.infoTextLabel?.text = "Проявка \(configuration.devTime), стоп-раствор \(configuration.stopTime), фикс \(configuration.fixTime), промывка \(configuration.washTime), сумка \(configuration.dryTime)"
+        
+        let cellShadowLayer : UIView = UIView(frame: CGRect(x: 19, y: 10, width: self.view.frame.size.width-38, height: 65)) 
+        cellShadowLayer.layer.backgroundColor = UIColor.white.cgColor
+        cellShadowLayer.layer.masksToBounds = false
+        cellShadowLayer.layer.cornerRadius = 10.0
+        cellShadowLayer.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        cellShadowLayer.layer.shadowRadius = 7
+        cellShadowLayer.layer.shadowOpacity = 0.4
+        
+        cell.addSubview(cellShadowLayer)
+        cell.sendSubview(toBack: cellShadowLayer)
         
         return cell
     }
